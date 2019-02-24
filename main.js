e51de0d75929744ffa29314a792019bfcf7a21e7
Luiz Daniel Lima <luizdml@gmail.com>
2019-02-24 10:23:20 -0300

    working version, brought from Codepen

---
+++ main.js
@@ -0,0 +1,136 @@
+window.onload = function() {
+  //when the user clicks in the input field
+  var numInput = document.getElementById("numInput");
+  var myButton = document.getElementById("button");
+  myButton.addEventListener("click", ShowResult);
+  numInput.addEventListener("click", selectAll);
+  numInput.addEventListener("keypress", checkKey);
+
+  function checkKey() {
+    //on pressing ENTER Key...
+    if (event.which === 13) {
+      ShowResult();
+    }
+  }
+
+  function selectAll() {
+    changeColorBackground();
+    this.focus();
+    this.setSelectionRange(0, this.value.length);
+  }
+  // lets show the result!
+  function ShowResult() {
+    var userInput = numInput.value;
+    if (userInput <= 3999) {
+      var romanNum = convertToRoman(userInput);
+      //var showResult = document.getElementById("showIt");
+      //showResult.innerHTML = romanNum;
+      //===commented the two lines above, testing DOM with JQuery===
+      $("#showIt").html(romanNum);
+      changeColorBackground();
+    } else {
+      $("#showIt").html("number is too high!");
+    }
+  }
+
+  var backColor = "light";
+
+  function changeColorBackground() {
+    //animates the input-div background color
+    var myDiv = $(".input-div");
+    if (backColor == "light") {
+      backColor = "dark";
+      myDiv.css("background-color", "#70C0B3");
+    } else if (backColor == "dark") {
+      backColor = "light";
+      myDiv.css("background-color", "#008F77");
+    }
+  }
+
+  //this part does all the math
+  function convertToRoman(num) {
+    var str = num.toString();
+
+    var romanU = [
+      "nono",
+      "I",
+      "II",
+      "III",
+      "IV",
+      "V",
+      "VI",
+      "VII",
+      "VIII",
+      "IX"
+    ];
+    var romanD = [
+      "nono",
+      "X",
+      "XX",
+      "XXX",
+      "XL",
+      "L",
+      "LX",
+      "LXX",
+      "LXXX",
+      "XC"
+    ];
+    var romanC = [
+      "nono",
+      "C",
+      "CC",
+      "CCC",
+      "CD",
+      "D",
+      "DC",
+      "DCC",
+      "DCCC",
+      "CM"
+    ];
+    var romanM = [
+      "nono",
+      "M",
+      "MM",
+      "MMM",
+      "MV",
+      "V",
+      "VM",
+      "VMM",
+      "VMMM",
+      "MX"
+    ];
+
+    var numArray = str.split("").reverse();
+
+    var tempArray = [];
+
+    for (i = 0; i < numArray.length; i++) {
+      if (numArray[i] != 0) {
+        switch (i) {
+          case 0:
+            roman = romanU[numArray[i]];
+            tempArray.push(roman);
+            break;
+          case 1:
+            roman = romanD[numArray[i]];
+            tempArray.push(roman);
+            break;
+          case 2:
+            roman = romanC[numArray[i]];
+            tempArray.push(roman);
+            break;
+          case 3:
+            roman = romanM[numArray[i]];
+            tempArray.push(roman);
+            break;
+        }
+      }
+    }
+
+    var answerArray = tempArray.reverse();
+
+    var answer = answerArray.join("");
+
+    return answer;
+  }
+};
